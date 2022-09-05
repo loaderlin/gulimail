84c9c4969e4b0637396c360ed8934a33f64ce297
loaderlin <loaderlin@163.com>
2022-09-05 23:43:30 +0800

    整合MyBatis-Plus

--- gulimail-product/src/main/java/com/atguigu/gulimall/product/GulimailProductApplication.java
+++ gulimail-product/src/main/java/com/atguigu/gulimall/product/GulimailProductApplication.java
@@ -3,6 +3,23 @@ package com.atguigu.gulimall.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1、整合Mybatis-plus
+ * 导入依赖
+ *         <!-- mybatis-plus -->
+ *         <dependency>
+ *             <groupId>com.baomidou</groupId>
+ *             <artifactId>mybatis-plus-boot-starter</artifactId>
+ *             <version>3.2.0</version>
+ *         </dependency>
+ *  配置
+ *      1、 配置数据源
+ *          导入数据库驱动
+ *          在application.yml配置数据源相关信息
+ *      2、 配置Mybatis-Plus
+ *          
+ *
+ */
 @SpringBootApplication
 public class GulimailProductApplication {
 
