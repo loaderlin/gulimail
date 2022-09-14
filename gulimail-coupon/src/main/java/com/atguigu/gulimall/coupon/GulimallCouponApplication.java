720e4789bef74f51c190c9266b8b3bed837f7107
loaderlin <loaderlin@163.com>
2022-09-15 00:22:29 +0800

    1、Spring Cloud Alibaba 版本降级 2.2.8.RELEASE -> 2.1.0.RELEASE 2、gulimall-common/pom.xml 结构调整 Spring-cloud-starter-alibaba-nacos-discovery调整出dependencyManagement 3、添加Nacos配置

--- gulimail-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimail-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -2,12 +2,14 @@ package com.atguigu.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class GulimailCouponApplication {
+public class GulimallCouponApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimailCouponApplication.class, args);
+        SpringApplication.run(GulimallCouponApplication.class, args);
     }
 
 }
