2c51f9028e3cd284712e4f5d4afef96ddc386baa
loaderlin <loaderlin@163.com>
2022-09-26 23:59:24 +0800

    Spring Cloud Nacos 配置命名空间，

--- gulimail-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimail-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -23,6 +23,27 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *      @Value("${配置项的名}") 获取到配置。
  *      如果配置中心和当前应用的配置文件中都配置了相同的项，优先使用配置中心的配置。
  */
+
+/**
+ * Nacos Config 配置空间
+ *
+ * 1、命名空间：配置隔离
+ *      默认：public（保留空间）；默认新增的所有配置都在public空间
+ *      一、开发；测试；生产：利用命名空间来做环境隔离。
+ *      注意：在bootstrap.properties；配置上，需要使用哪个命名空间下的配置
+ *      spring.cloud.nacos.config.namespace=94dd6bc9-b58e-4fb5-b0e4-7a8982b265b6
+ *      二、基于每一个微服务之间相互隔离配置，每个微服务都创建自己的命名空间，只加载自己命名空间下的所有配置
+ * 2、配置集
+ *      所有的配置的集合  application.yml application.properties里面的所有内容
+ * 3、配置集ID
+ *      类似配置文件名 Data ID：类似文件名
+ * 4、配置分组
+ *      默认所有的配置集都属于：DEFAULT_GROUP
+ *      dev,test,prod
+ * 每个微服务创建自己的命名空间，使用配置分组区分环境，dev，test，prod
+ */
+
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
